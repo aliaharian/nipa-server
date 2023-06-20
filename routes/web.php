b5792a3d4f22e5aee0e02d85fb2706d2d628775c
ali aharian <izadi.sa@nahad.ir>
2023-06-20 13:33:55 +0330

    bugfix

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get("/migrate", function () {
+    Artisan::call('migrate');
+    return 'ok';
+});
