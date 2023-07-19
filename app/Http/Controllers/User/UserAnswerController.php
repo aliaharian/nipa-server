009c567a299b38a9f572a77ff8ba22562ab492a5
ali aharian <izadi.sa@nahad.ir>
2023-07-19 11:56:15 +0330

    bugfix

--- app/Http/Controllers/User/UserAnswerController.php
+++ app/Http/Controllers/User/UserAnswerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\BasicData;
 use App\Models\Form;
 use App\Models\Order;
 use App\Models\UserAnswer;
@@ -11,7 +12,7 @@ use Illuminate\Http\Request;
 class UserAnswerController extends Controller
 {
     //
-           /**
+    /**
      * @OA\Post(
      *  path="/v1/userAnswer/{form_id}/answer",
      * tags={"UserAnswer"},
@@ -25,7 +26,7 @@ class UserAnswerController extends Controller
      * format="int64"
      * )
      * ),
-    * @OA\RequestBody(
+     * @OA\RequestBody(
      * required=true,
      * @OA\JsonContent(
      * required={"order_id"},
@@ -43,18 +44,20 @@ class UserAnswerController extends Controller
      * )
      * )
      */
-    public function userAnswerForm($form_id , Request $request){
+    public function userAnswerForm($form_id, Request $request)
+    {
         $form = Form::find($form_id);
-        if(!$form){
-            return response()->json(['message'=>'form not found'], 404);
+        if (!$form) {
+            return response()->json(['message' => 'form not found'], 404);
         }
         $user = $request->user();
         //check if user role inside form role
         $user_roles = $user->roles()->pluck('role_id')->toArray();
-        $form_roles = $form->roles()->pluck('role_id')->toArray();
-        $intersect = array_intersect($user_roles , $form_roles);
-        if(count($intersect) == 0){
-            return response()->json(['message'=>'user not allowed to answer this form'], 403);
+        $product_step = $form->productSteps[0];
+        $form_roles = $product_step->roles()->pluck('role_id')->toArray();
+        $intersect = array_intersect($user_roles, $form_roles);
+        if (count($intersect) == 0) {
+            return response()->json(['message' => 'user not allowed to answer this form'], 403);
         }
         //check if user created this order if not admin
 
@@ -62,30 +65,39 @@ class UserAnswerController extends Controller
         //validate fields if required  
         $requirements = array();
         $requirements['order_id'] = 'required|exists:orders,id';
-        foreach($fields as $field){
-            $tmp="";
-            if($field->required){
-                $tmp.="required";
+        foreach ($fields as $field) {
+            $tmp = "";
+            if ($field->required) {
+                $tmp .= "required";
             }
-            if($field->type->validations){
-                $tmp.= '|'.$field->type->validations;
+            if ($field->type->validations) {
+                $tmp .= '|' . $field->type->validations;
             }
-            if($field->type->has_options==1){
-                $options=array();
-                foreach($field->options as $option){
-                    $options[]=$option->option;
+            if ($field->type->has_options == 1) {
+                $options = array();
+                //check if from basic datas or from options?
+                if ($field->basic_data_id) {
+                    $basicData = BasicData::find($field->basic_data_id);
+                    foreach ($basicData->items as $option) {
+                        $options[] = $option->code;
+                    }
+                } else {
+                    foreach ($field->options as $option) {
+                        $options[] = $option->option;
+                    }
                 }
-                $tmp.= '|in:'.implode(',' , $options);
+
+                $tmp .= '|in:' . implode(',', $options);
             }
+
 
             $requirements[$field->name] = $tmp;
         }
-
         $data = $request->validate($requirements);
-       
+
         $formStep = $form->productSteps()->first();
-        if(!$formStep){
-            return response()->json(['message'=>'form has no steps'], 404);
+        if (!$formStep) {
+            return response()->json(['message' => 'form has no steps'], 404);
         }
         // $formStepMeta = json_decode($formStep->meta);
         // if($formStepMeta->first_step == 'true'){
@@ -95,18 +107,33 @@ class UserAnswerController extends Controller
         //     ]);
         // }
         //create user answer
-       foreach($fields as $field){
-            $userAnswer = UserAnswer::updateOrCreate([
-                'user_id'=>$user->id,
-                'form_field_id'=>$field->id,
-                'order_id'=>$data['order_id'],
-            ],[
-                'answer'=>$request[$field->name],
-            ]);
+        foreach ($fields as $field) {
+            if (gettype($request[$field->name]) == 'array') {
+                //remove prev...
+                UserAnswer::where("user_id", $user->id)->where("form_field_id", $field->id)->where("order_id", $data['order_id'])->delete();
+                foreach ($request[$field->name] as $fieldValue) {
+                    $userAnswer = UserAnswer::create([
+                        'user_id' => $user->id,
+                        'form_field_id' => $field->id,
+                        'order_id' => $data['order_id'],
+                        'answer' => $field->type->type == "uploadFile" ? $fieldValue["hashCode"] : $fieldValue
+                    ]);
+
+                }
+
+            } else {
+                $userAnswer = UserAnswer::updateOrCreate([
+                    'user_id' => $user->id,
+                    'form_field_id' => $field->id,
+                    'order_id' => $data['order_id'],
+                ], [
+                    'answer' => $request[$field->name],
+                ]);
+            }
         }
 
-        $userAnswer = UserAnswer::where('user_id',$user->id)->where('order_id',$data['order_id'])->get();
+        $userAnswer = UserAnswer::where('user_id', $user->id)->where('order_id', $data['order_id'])->get();
 
-        return response()->json(["userAnswer"=>$userAnswer], 200);
+        return response()->json(["userAnswer" => $userAnswer], 200);
     }
 }
